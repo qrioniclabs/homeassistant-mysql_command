5e981f1c2b42786bd3e3f72c685d249317a4c8f9
qrionic labs <info@qrioniclabs.com>
2023-10-15 20:19:21 +0200

    Update const.py

--- custom_components/mysql_command/const.py
+++ custom_components/mysql_command/const.py
@@ -1,8 +1,8 @@
 CONF_MYSQL_HOST = "host"
+CONF_MYSQL_PORT = "port"
 CONF_MYSQL_USERNAME = "username"
 CONF_MYSQL_PASSWORD = "password"
 CONF_MYSQL_DB = "db"
-CONF_MYSQL_PORT = "port"
 CONF_MYSQL_TIMEOUT = "timeout"
 
 # Defaults
